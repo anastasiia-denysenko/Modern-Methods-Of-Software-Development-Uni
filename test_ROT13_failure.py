7372d81a1681424e29a5c7526c37f63af3c7bdc5
anastasiia-denysenko <145994553+anastasiia-denysenko@users.noreply.github.com>
2024-04-12 00:58:37 +0300

    Update test_ROT13_failure.py

--- test_ROT13_failure.py
+++ test_ROT13_failure.py
@@ -1,4 +1,4 @@
 from denysenko_ROT13 import ROT13, test
-def test_function1_faliure():
+def test_function_faliure():
     if test() == "incorrect":
         pytest.fail("The function works incorrectly.")
