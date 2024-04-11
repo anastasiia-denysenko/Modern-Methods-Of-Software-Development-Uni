0b5bfbd6072296551e3e156bdb2e7a3356db0d46
anastasiia-denysenko <denysenkoanastasia24@gmail.com>
2024-04-12 01:10:03 +0300

    delete unnecessary

--- lab_1_1/test_ROT13_failure.py
+++
@@ -1,4 +0,0 @@
-from denysenko_ROT13 import ROT13, test
-def test_function1_faliure():
-    if function("Text for test") == "incorrect":
-        pytest.fail("The function works incorrectly.")
--- lab_1_1/test_ROT13_seccess.py
+++
@@ -1,3 +0,0 @@
-from denysenko_ROT13 import ROT13, test
-def test_function():
-    assert test("Text for test") == "correct"
