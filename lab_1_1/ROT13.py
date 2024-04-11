dc5ff7008804abfbb4ab8bb87b1293dfccf71b7d
anastasiia-denysenko <denysenkoanastasia24@gmail.com>
2024-04-12 00:14:13 +0300

    Add ROT13

---
+++ lab_1_1/ROT13.py
@@ -0,0 +1,46 @@
+import random
+import sys
+from multiprocessing import Process
+upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+lower = []
+for i in range (len(upper)):
+    lower.append((upper[i]).lower())
+def ROT13(the_text):
+    encoded = ''
+    for i in the_text:
+        for j in range (26):
+            if i == upper[j]:
+                if j+13<=26:
+                    encoded+=str(upper[j+13])
+                if j+13>26:
+                    encoded+=str(upper[j+13-26])
+            if i == lower[j]:
+                if j+13<=26:
+                    encoded+=str(lower[j+13])
+                if j+13>26:
+                    encoded+=str(lower[j+13-26])
+        if (i not in upper)&(i not in lower):
+            encoded+=str(i)
+    return encoded
+print("Input: Hello world!")
+print("Applying ROT13:", ROT13('Hello world!'))
+print("Applying ROT13 for the second time:", ROT13(ROT13('Hello world!')))
+def test():
+    test_text = 'Hello world!'
+    if ROT13(ROT13(test_text)) == test_text:
+        return "correct"
+        exit(0)
+    else:
+        return "incorrect"
+        exit(1)
+if test() == "correct":
+    pass
+else:
+    print("An error occured", file=sys.stderr)
+if __name__ == '__main__':
+    test_process = Process(target=test)
+    test_process.start()
+    test_process.join()
+    code = test_process.exitcode
+    print(f'Exit code: {code}')
+
