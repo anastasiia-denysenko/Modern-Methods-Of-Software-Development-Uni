e19d7f9beb947c3d11eedfb0dcebc11b200f5160
anastasiia-denysenko <145994553+anastasiia-denysenko@users.noreply.github.com>
2024-04-13 21:27:38 +0300

    Update denysenko_ROT13.py

--- lab_2/denysenko_ROT13.py
+++ lab_2/denysenko_ROT13.py
@@ -1,4 +1,3 @@
-import random
 import sys
 from multiprocessing import Process
 upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
