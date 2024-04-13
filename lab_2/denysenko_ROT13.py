56e74a4cfc2136a77e2f19106b09f6fe3846d7b5
anastasiia-denysenko <145994553+anastasiia-denysenko@users.noreply.github.com>
2024-04-13 23:58:09 +0300

    Update denysenko_ROT13.py

--- lab_2/denysenko_ROT13.py
+++ lab_2/denysenko_ROT13.py
@@ -1,5 +1,6 @@
 import sys
 from multiprocessing import Process
+import io
 upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 lower = []
 for i in range (len(upper)):
@@ -21,19 +22,27 @@ def ROT13(the_text):
         if (i not in upper)&(i not in lower):
             encoded+=str(i)
     return encoded
-print("Input: Hello world!")
-print("Applying ROT13:", ROT13('Hello world!'))
-print("Applying ROT13 for the second time:", ROT13(ROT13('Hello world!')))
+input = 'Hello world!'
+in_stream = io.BytesIO(input.encode('utf-8'))
+sys.stdin = io.TextIOWrapper(in_stream, encoding='utf-8')
 def test():
-    test_text = 'Hello world!'
-    if ROT13(ROT13(test_text)) == test_text:
+    if ROT13(ROT13(input)) == input:
         return "correct"
         exit(0)
     else:
         return "incorrect"
         exit(1)
-if test() == "incorrect":
-    print("An error occured", file=sys.stderr)
+def stdin_stderr():
+    if test() == "correct":
+        temp = sys.stdout                        
+        sys.stdout = open('ouput.txt', 'w')
+        print(ROT13(input))
+        print(ROT13(ROT13(input)))
+        sys.stdout.close()
+        sys.stdout = temp
+    else:
+        sys.stderr.write(f"The function does not work\n")
+stdin_stderr()
 if __name__ == '__main__':
     test_process = Process(target=test)
     test_process.start()
