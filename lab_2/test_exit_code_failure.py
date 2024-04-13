ab81171329defe40872f40f39c7311b3d6381cc4
anastasiia-denysenko <145994553+anastasiia-denysenko@users.noreply.github.com>
2024-04-13 21:25:13 +0300

    Update test_exit_code_failure.py

--- lab_2/test_exit_code_failure.py
+++ lab_2/test_exit_code_failure.py
@@ -1,4 +1,3 @@
 from denysenko_ROT13 import ROT13, test, code
-def test_exit_code_faliure():
-    if code != 0:
-        pytest.fail("Exit code does not equal 0!")
+def exit_code_one(self):
+    assert self.assertEqual(code, 1)
