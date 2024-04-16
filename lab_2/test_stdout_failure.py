634dd4afb3e6aecdfca61f3e181ce57750282990
anastasiia-denysenko <denysenkoanastasia24@gmail.com>
2024-04-16 22:20:59 +0300

    delete unnecessery

--- lab_2/test_stdout_failure.py
+++
@@ -1,6 +0,0 @@
-import denysenko_ROT13 as rot
-import sys
-output = str(rot.ROT13(rot.input))+"\n"+str(rot.ROT13(rot.ROT13(rot.input)))
-def test_stdin_fail():
-    if output != rot.sys.stdout.read():
-        pytest.fail("Output is not written to stdout.")
