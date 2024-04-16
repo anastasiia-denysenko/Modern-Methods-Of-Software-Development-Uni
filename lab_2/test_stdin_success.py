c0bf2eed7a9cf2ba5898215d5f4459735aae97a5
anastasiia-denysenko <145994553+anastasiia-denysenko@users.noreply.github.com>
2024-04-16 11:19:29 +0300

    Update test_stdin_success.py

--- lab_2/test_stdin_success.py
+++ lab_2/test_stdin_success.py
@@ -1,4 +1,4 @@
 from denysenko_ROT13 import in_stream
-def test_double(self):
-    self.setattr('sys.stdin', in_stream)
+def test_double(monkeypatch):
+    monkeypatch.setattr('sys.stdin', in_stream)
     assert in_stream == 'Hello world!'
