661bc7a1823a8ccfb5e9c0ae879a235a003ffd70
anastasiia-denysenko <145994553+anastasiia-denysenko@users.noreply.github.com>
2024-04-16 21:39:35 +0300

    Update test_stdout_success.py

--- lab_2/test_stdout_success.py
+++ lab_2/test_stdout_success.py
@@ -2,4 +2,5 @@ import denysenko_ROT13 as rot
 import sys
 output = str(rot.ROT13(rot.input))+"\n"+str(rot.ROT13(rot.ROT13(rot.input)))
 def test_stdout_success():
-    assert output == rot.sys.stdout.read()
+    captured = capsys.readouterr()
+    assert captured.out == output
