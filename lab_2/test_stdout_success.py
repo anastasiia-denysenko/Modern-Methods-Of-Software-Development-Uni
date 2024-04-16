a5d5f13d33cf92f138f61e25006ccae6aec4939a
anastasiia-denysenko <145994553+anastasiia-denysenko@users.noreply.github.com>
2024-04-16 22:11:45 +0300

    Update test_stdout_success.py

--- lab_2/test_stdout_success.py
+++ lab_2/test_stdout_success.py
@@ -3,13 +3,12 @@ import sys
 import subprocess
 from io import StringIO
 from contextlib import redirect_stdout, redirect_stderr
+output = str(ROT13(input))+"\n"+str(ROT13(ROT13(input)))
 def stdout_stderr(self, string_input, expection_stdout, expection_stderr):
     with StringIO() as stdout_buf, StringIO() as stderr_buf:
         with redirect_stdout(stdout_buf), redirect_stderr(stderr_buf):
             result = subprocess.run(["python", "lab2/denysenko_ROT13.py"], input=string_input, text=True, capture_output=True)
         captured_stdout = result.stdout
         captured_stderr = result.stderr
-    return list(captured_stdout, captured_stderr)
-output = str(ROT13(input))+"\n"+str(ROT13(ROT13(input)))
-def test_stdout_success():
-    assert stdout_stderr(self, string_input, expection_stdout, expection_stderr)[0] == output
+    assert captured_stdout == output
+    assert captured_stderr == ""
