75fa60eb25fa5d854468749d15d06934627e1181
anastasiia-denysenko <145994553+anastasiia-denysenko@users.noreply.github.com>
2024-05-18 22:30:53 +0300

    Update rot13denysenko.py

--- lab_2/rot13denysenko.py
+++ lab_2/rot13denysenko.py
@@ -1,4 +1,3 @@
-import sys
 upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 lower = []
 for i in range (len(upper)):
