6b973bbfd6429acbaa56de83f3d935a06c2f0893
anastasiia-denysenko <denysenkoanastasia24@gmail.com>
2024-05-18 22:17:53 +0300

    add_updateded_ROT13

---
+++ lab_2/ROT_13_test.py
@@ -0,0 +1,31 @@
+import denysenko_ROT13
+import unittest
+import sys
+import unittest.mock
+class TestROT13(unittest.TestCase):
+    def test_if_function_works(self):
+        self.assertEqual(ROT_13().ROT13(ROT_13().ROT13("Hello world")), "Hello world")
+    def test_exit_code(self):
+        with self.assertRaises(SystemExit) as cm:
+            if ROT_13().ROT13(ROT_13().ROT13("Hello world")) == "Hello world":
+                sys.stdout.write("Hello world!")
+                sys.exit(0)
+            else:
+                print('The function does not work', file=sys.stderr)
+                sys.exit(1)
+            the_exception = cm.exception
+            self.assertEqual(the_exception.code, 0)
+    def test_stdin(self):
+        denysenko_ROT13.raw_input = lambda _: 'Hello world!'
+        ng = denysenko_ROT13.ROT_13()
+        ng.get_name()
+        self.assertEquals(ng.name, 'Hello world!')
+class TestStdoutStderr(unittest.TestCase):
+    def test_stdout(self, mockStdout):
+        self.TestROT13.test_exit_code()
+        mockStdout.write.assert_has_calls([
+            mock.call("Hello world!")
+        ])
+    def test_stderr(self, mockStdout):
+        self.TestROT13.test_exit_code()
+        mockStdout.assert_not_called()
---
+++ lab_2/rot13denysenko.py
@@ -0,0 +1,27 @@
+import sys
+upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+lower = []
+for i in range (len(upper)):
+    lower.append((upper[i]).lower())
+class ROT_13:
+    def get_text(self):
+       return self.the_text
+    def get_name(self):
+        self.name = input('Hello world!')
+    def ROT13(self, the_text):
+        encoded = ''
+        for i in the_text:
+            for j in range (26):
+                if i == upper[j]:
+                    if j+13<=26:
+                        encoded+=str(upper[j+13])
+                    if j+13>26:
+                        encoded+=str(upper[j+13-26])
+                if i == lower[j]:
+                    if j+13<=26:
+                        encoded+=str(lower[j+13])
+                    if j+13>26:
+                        encoded+=str(lower[j+13-26])
+            if (i not in upper)&(i not in lower):
+                encoded+=str(i)
+        return encoded
