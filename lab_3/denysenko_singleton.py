22fdee2907f0ddc3f69ed10131527abb51d0e70f
anastasiia-denysenko <145994553+anastasiia-denysenko@users.noreply.github.com>
2024-05-18 19:10:38 +0300

    add_lab_3

---
+++ lab_3/denysenko_singleton.py
@@ -0,0 +1,9 @@
+class Singleton:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+    def result(self, x, y):
+        print(x+y)
