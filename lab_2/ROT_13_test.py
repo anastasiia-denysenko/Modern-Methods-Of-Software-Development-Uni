9963a8fc63e5010708da745e54eabd8bc469c30f
anastasiia-denysenko <145994553+anastasiia-denysenko@users.noreply.github.com>
2024-05-23 21:15:14 +0300

    Update ROT_13_test.py

--- lab_2/ROT_13_test.py
+++ lab_2/ROT_13_test.py
@@ -4,7 +4,7 @@ import sys
 def test_stdout_stderr(capsys):
     otput_rot13("Hello world!")
     captured = capsys.readouterr()
-    assert captured.out == "Uryyb jbeyq!"
+    assert captured.out == "The output is: Uryyb jbeyq!"
     assert captured.err == "Starting process...\n"
 def test_exit_code(ROT_13.ROT13("Hello world!")):
     with pytest.raises(SystemExit) as e:
