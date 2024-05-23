f52626918f2fb49fbee1bc5ac0a35aefaf1c6f7a
anastasiia-denysenko <denysenkoanastasia24@gmail.com>
2024-05-23 19:56:41 +0300

    create new files

--- lab_2/rot13denysenko.py
+++ lab_2/rot13denysenko.py
@@ -1,12 +1,9 @@
+import sys
 upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 lower = []
 for i in range (len(upper)):
     lower.append((upper[i]).lower())
 class ROT_13:
-    def get_text(self):
-       return self.the_text
-    def get_name(self):
-        self.name = input('Hello world!')
     def ROT13(self, the_text):
         encoded = ''
         for i in the_text:
@@ -24,3 +21,13 @@ class ROT_13:
             if (i not in upper)&(i not in lower):
                 encoded+=str(i)
         return encoded
+def otput_rot13(s:str):
+    m = ROT_13()
+    if (m.ROT13(m.ROT13(s))) == s:
+        sys.stderr.write("Starting process...\n")
+        sys.stdout.write("The output is: ")
+        sys.stdout.write(m.ROT13(s))
+        sys.exit(0)
+    else:
+        sys.stderr.write("Process terminated: function does not work.")
+        sys.exit(1)
