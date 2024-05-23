e8c839e8bf138bf1b8ad85a568d187734fe31c05
anastasiia-denysenko <denysenkoanastasia24@gmail.com>
2024-05-23 13:22:55 +0300

    add files

---
+++ lab_4/ref_and_var.cpp
@@ -0,0 +1,21 @@
+
+class ref_and_point_zero
+{
+    public:
+        int x = 5;
+        const int& ref = x;
+};
+class ref_and_point_one
+{
+    public:
+        int x = 10;
+};
+class ref_and_point_two
+{
+    public:
+       const int& ref = 20;
+};
+int main()
+{
+  return 0;
+}
---
+++ lab_4/test_ref_and_var.cpp
@@ -0,0 +1,17 @@
+# include <assert.h>
+#include <iostream>
+#include <cassert>
+#define assertm(exp, msg) assert(((void)msg, exp))
+
+int main()
+{
+    ref_and_point_zero rp_zero;
+    ref_and_point_one rp_one;
+    ref_and_point_two rp_two;
+    assert(rp_zero.x == rp_zero.ref);
+    std::cout << "Checkpoint #1\n";
+    assert(rp_one.x != rp_one.ref);
+    std::cout << "Checkpoint #2\n";
+    assert(rp_two.x == rp_two.ref);
+    std::cout << "Checkpoint #3\n";
+}
