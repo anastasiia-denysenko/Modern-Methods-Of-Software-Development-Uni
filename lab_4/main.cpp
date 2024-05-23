07fd25934ec848d3b58c3f2ed5df58fcf87a21c7
anastasiia-denysenko <145994553+anastasiia-denysenko@users.noreply.github.com>
2024-05-23 22:54:00 +0300

    Update main.cpp

--- lab_4/main.cpp
+++ lab_4/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <assert.h>
-#include "ref_and_var.h"
+#include "ref_and_var.h"// include file with original values
 using std::cout;
 int main()
 {
-    x_and_ref x_ref;
-    x_ref.x = 7;
+    x_and_ref x_ref;// include class from ref_and_var.h
+    x_ref.x = 7;// change value of variable x, so reference also changes
     assert(x_ref.ref==7); // returns true
-    x_ref.ref = 10;
+    x_ref.ref = 10;// change value of reference, which prescribes x new value
     assert(x_ref.x==10); // returns true
     return 0;
 }
