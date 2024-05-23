92baec0760e784414ef669307b2e5b8555e781f5
anastasiia-denysenko <145994553+anastasiia-denysenko@users.noreply.github.com>
2024-05-23 22:41:49 +0300

    Update main.cpp

--- lab_4/main.cpp
+++ lab_4/main.cpp
@@ -7,9 +7,7 @@ int main()
     x_and_ref x_ref;
     x_ref.x = 7;
     assert(x_ref.ref==7); // returns true
-    std::cout << "Checkpoint #1\n";
     x_ref.ref = 10;
     assert(x_ref.x==10); // returns true
-    std::cout << "Checkpoint #2\n";
     return 0;
 }
