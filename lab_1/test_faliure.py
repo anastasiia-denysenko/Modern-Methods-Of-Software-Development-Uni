77fbf71dd75c8d01c704528df37581384e1c9305
anastasiia-denysenko <145994553+anastasiia-denysenko@users.noreply.github.com>
2024-05-25 10:28:45 +0300

    delete unneccessery

--- lab_1/test_faliure.py
+++
@@ -1,8 +0,0 @@
-from dumb_code_denysenko import function1, function2
-def test_function1_faliure():
-    if function1(1) == "incorrect":
-        pytest.fail("The function works incorrectly.")
-
-def test_function2_faliure():
-    if function2(2) == "incorrect":
-        pytest.fail("The function works incorrectly.")
--- lab_3/denysenko_singleton.py
+++
@@ -1,9 +0,0 @@
-class Singleton:
-    _instance = None
-
-    def __new__(cls, *args, **kwargs):
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
-        return cls._instance
-    def result(self, x, y):
-        print(x+y)
