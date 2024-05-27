97942b0a2764d4f0befca6160f6c2dad18a11165
anastasiia-denysenko <145994553+anastasiia-denysenko@users.noreply.github.com>
2024-05-27 15:09:36 +0300

    add folder tests

--- test_sorting.py
+++
@@ -1,8 +0,0 @@
-import plot_line
-import pytest
-def test_sort():
-    a = random.sample(range(-1000, 1000), 10)
-    n = a.copy()
-    a.sort()
-    quick_sort(n, 0, len(n)-1)
-    assert a==n
---
+++ tests/test_sorting.py
@@ -0,0 +1,13 @@
+import plot_line
+import unittest
+class Test(unittest.TestCase):
+
+    def test_sort(self):
+        a = random.sample(range(-1000, 1000), 10)
+        n = a.copy()
+        a.sort()
+        quick_sort(n, 0, len(n)-1)
+        self.assertEqual(a, n)
+
+if __name__ == '__main__':
+    unittest.main()
