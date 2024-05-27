912cc1cc66d9509df3858386ee64b3665bf6b18e
anastasiia-denysenko <145994553+anastasiia-denysenko@users.noreply.github.com>
2024-05-27 15:43:34 +0300

    Update test_sorting.py

--- test_sorting.py
+++ test_sorting.py
@@ -3,11 +3,11 @@ import unittest2
 import random
 class Test(unittest2.TestCase):
     def test_sort(self):
-    a = random.sample(range(-1000, 1000), 10)
-    n = a.copy()
-    a.sort()
-    quick_sort(n, 0, len(n)-1)
-    self.assertEqual(a,n)
+        a = random.sample(range(-1000, 1000), 10)
+        n = a.copy()
+        a.sort()
+        quick_sort(n, 0, len(n)-1)
+        self.assertEqual(a,n)
 
 if __name__ == '__main__':
     unittest2.main()
